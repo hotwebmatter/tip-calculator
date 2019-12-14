5fd5eef4e69730520aa7786f16e2340e07510c6a
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-13 19:36:57 -0500

    Declare constants and vars, read total bill

--- Program.cs
+++ Program.cs
@@ -7,7 +7,29 @@ namespace tip_calculator
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            double billCharge,
+                   tipCharge,
+                   totalCharge;
+            const double LOW_TIP = 0.15,
+                         GOOD_TIP = 0.2;
+
+            // Compile-time initialization is not specified
+            Write("Total bill charge: ");
+            billCharge = ReadCharge();
+            WriteLine("{0, 5} {1:C2}", "Bill:", billCharge);
+        }
+
+        static double ReadCharge()
+        {
+            double charge;
+            string userInput = ReadLine();
+            while (double.TryParse(userInput, out charge) == false)
+            {
+                WriteLine("Invalid input! Use dollars and cents.");
+                Write("Total bill charge: ");
+                userInput = ReadLine();
+            }
+            return charge;
         }
     }
 }
